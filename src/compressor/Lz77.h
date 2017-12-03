d56a52ee5b3af37284c9afd7b30a19f07d450821
gaabs <gaabs@cin.ufpe.br>
2017-12-03 10:32:09 -0300

    Update lz77 to allow bigger buffer

--- src/compressor/Lz77.h
+++ src/compressor/Lz77.h
@@ -8,8 +8,8 @@ using namespace std;
 
 class Lz77 : public Compressor {
 private:
-    int bufferSize = 255;
-    int lookaheadSize = 255;
+    int bufferSize = 4095;
+    int lookaheadSize = 15;
 public:
     Lz77() {}
 
@@ -20,9 +20,15 @@ public:
         tuple<int, int, char> occurrence;
         while (i < strSize) {
             occurrence = patternMatch(str, strSize, i, bufferSize, lookaheadSize);
-            encoded.push_back(get<0>(occurrence));
-            encoded.push_back(get<1>(occurrence));
-            encoded.push_back(get<2>(occurrence));
+            int matchPos = get<0>(occurrence);
+            int matchSize = get<1>(occurrence);
+            char firstByte = (char) (matchPos >> 4);
+            char secondByte = (char) ((((0x000F & matchPos) << 4)) | matchSize);
+
+            encoded.push_back(firstByte);
+            encoded.push_back(secondByte);
+            encoded.push_back((char) get<2>(occurrence));
+
             i += get<1>(occurrence) + 1;
         }
 
@@ -34,8 +40,11 @@ public:
 
         int pos = 0;
         for (int i = 0; i < encodedSize; i += 3) {
-            int size = (unsigned char) encoded[i + 1];
-            int st = pos - (unsigned char) encoded[i];
+            int firstByte = (unsigned char) encoded[i];
+            int secondByte = (unsigned char) encoded[i + 1];
+
+            int size = (unsigned char) secondByte & 0x000F;
+            int st = pos - (((unsigned char) firstByte << 4) | ((unsigned char) secondByte >> 4));
             int en = st + size;
 
             for (int j = st; j < en; j++) {
@@ -51,7 +60,8 @@ public:
 
 private:
     tuple<int, int, char> patternMatch(char *str, int strSize, int lookStart, int bufferSize, int lookSize) {
-        int lookEnd = min(int(strSize - 1), lookStart + lookSize);
+        int lookEnd = min(strSize - 1, lookStart + lookSize);
+//        int lookEnd = min(int(strSize - 1), lookStart + lookSize); // pq o cast muda o tamanho da compressao?
         int bufferStart = max(0, lookStart - bufferSize);
         int bufferEnd = lookStart;
 
@@ -68,6 +78,10 @@ private:
             if (size > biggestMatchSize) {
                 biggestMatchSize = size;
                 biggestMatchPos = i;
+            }
+
+            if (size == lookSize) {
+                break;
             }
         }
 
