a9fca4a95b326afa72664cf78099633a523d7b74
gaabs <gaabs@cin.ufpe.br>
2017-12-08 12:32:57 -0300

    Update compressor test

--- src/compressor/CompressorTest.cpp
+++ src/compressor/CompressorTest.cpp
@@ -13,7 +13,6 @@ using namespace std;
 
 #define DEBUG 0
 
-// LZ77
 int main(int argc, char *argv[]) {
     Compressor *compressor = nullptr;
     string compressorName = argv[1];
@@ -41,11 +40,8 @@ int main(int argc, char *argv[]) {
                encoded.size(),
                inputVector.size(),
                double(encoded.size()) / inputVector.size());
-        printf("time = %d secs\n", t1 - t0);
+        printf("compress time = %d secs\n", t1 - t0);
 
-//    vector<char> decoded = compressor->decode(&encoded[0], encoded.size());
-//    printf("decoded size:%d\n", decoded.size());
-//    FileUtils::writeBytes(fileOutStream, decoded);
     } else if (string(argv[3]).compare("decompress") == 0) {
         int t0 = time(NULL);
         ifstream fileStream(filename + ".myzip");
@@ -53,7 +49,19 @@ int main(int argc, char *argv[]) {
         vector<char> decoded = compressor->decode(&inputVector[0], inputVector.size());
         int t1 = time(NULL);
 
-        printf("time = %d secs\n", t1 - t0);
+        printf("decompress time = %d secs\n", t1 - t0);
+    } else if (string(argv[3]).compare("validate") == 0) {
+        int t0 = time(NULL);
+        ifstream fileStream(filename);
+        vector<char> inputVector = FileUtils::readBytes(fileStream);
+        vector<char> encoded = compressor->encode(&inputVector[0], inputVector.size());
+        vector<char> decoded = compressor->decode(&encoded[0], encoded.size());
+
+        for (int i = 0; i < decoded.size(); i++) {
+            assert(decoded[i] == inputVector[i]);
+        }
+
+        printf("original size:%d decoded size:%d\n", inputVector.size(), decoded.size());
     } else {
         puts("invalid mode");
         return 1;
